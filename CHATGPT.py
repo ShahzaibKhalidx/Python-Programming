7c59389ee2532c2f0feae1ac58d0a98bf6b98329
ShahzaibKhalidx <shahzaib1638@gmail.com>
2024-05-22 22:56:43 +0500

    2nd assignment

---
+++ CHATGPT.py
@@ -0,0 +1,12 @@
+value_list = []
+
+while True:
+    value = input("enter a value ")
+    value_list.append(value)
+
+print(value_list)
+
+
+        
+    
+     
--- iterative_structure.py
+++ iterative_structure.py
@@ -23,11 +23,11 @@ for x in range(1,11):
 
 numbers = [12, 75, 150, 180, 145, 525, 50]
 for i in numbers:
-    if i > 500:
-        break
-    if i > 150:
-        continue
-    if  i%5==0:
+    if i % 5 == 0:
+        if i > 150:
+            continue
+        if i > 500:
+            break
         print(i)
 
 
@@ -62,11 +62,24 @@ print(factorial)
 
 # TASK-8
 
-
+num = int(input("Enter a number "))
+thesum = 0
+for i in range(1,  num+1):
+    thesum += i
+print(thesum)
 
 # TASK-9
 
+import random
+ran_num = random.randint(1,9)
 
+while True:
+    inp = int(input("Guess a number! "))
+    if inp == ran_num:
+        print("Well guessed", ran_num)
+        break
+    else:
+        inp = int(input("Try again "))
 
 # TASK-10
 
@@ -74,17 +87,42 @@ word = str(input("Enter a word "))
 reverse = word[::-1]
 print(reverse)
 
+
+# TASK-11
+
+inp = str(input("Enter the word "))
+letters = []
+digits = []
+
+for x in inp:
+    if x.isalpha():
+        letters.append(x)
+    else:
+        digits.append(x)
+        
+print(f"letters={letters}, digits={digits}") 
+
+
+# TASK-12
+
+inp = input("Enter the word ")
+count = 0
+for x in inp:
+    count += 1
+
+print("Word count: ",count)
+
 # TASK-13
-name = "muhammad shahzaib"
-vowels = 0
-consonants = 0
+name = input("Enter name ")
+vowels = []
+consonants = []
 for x in name:
-    for i in ["a","e","i","o","u"]:
-        if x == i:
-            vowels += 1
-        else:
-            consonants = len(name) - vowels
-print(f"vowels={vowels} and consonants={consonants}")
+    if x.lower() in 'aeiou':
+        vowels.append(x)
+    elif x.isalpha():
+        consonants.append(x)
+
+print(f"vowels={len(vowels)} and consonants={len(consonants)}")
     
 
 # TASK-14
@@ -100,3 +138,63 @@ print(value_list)
 
 # TASK-15
 
+list1 = ["python","java","c++","javascript","swift"]
+list2 = []
+for x in list1:
+    list2.append(x)
+
+print(list2)
+
+
+# TASK-16
+
+lst_num = [11,25,2,6,63,45]
+lst_num.sort()
+lst_num.reverse()
+print(lst_num[0])
+
+
+# TASK-17
+
+numbers = [1,2,3,4,5,6,7,8,9]
+even = []
+odd = []
+for x in numbers:
+    if x % 2 == 0:
+        even.append(x)
+    else:
+        odd.append(x)
+
+print("Even: ", len(even), "Odd: ", len(odd))
+
+
+# TASK-18
+
+numbers = [3,5,23,6,5,1,2,9,8]
+summ = 0
+for x in numbers:
+    summ += (x**2)
+
+print(summ)
+
+
+# TASK-19
+
+gadgets = ["Mobile", "Laptop", 100, "Camera", 310.28, "Speakers", 
+27.00, "Television", 1000, "Laptop Case", "Camera Lens"]
+
+strings = []
+digits = []
+
+for x in gadgets:
+    if type(x) == str:
+        strings.append(x)
+    else:
+        digits.append(x)
+        
+digits.sort()
+digits_reverse = digits.copy()
+digits_reverse.reverse()
+strings.sort()
+strings_reverse = strings.copy()
+strings_reverse.reverse()
